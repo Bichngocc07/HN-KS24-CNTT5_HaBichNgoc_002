de4fbe19228b7dae25a2bb7b116377d5744a3d9a
Bichngocc07 <ngoc01072006@gmail.com>
2026-03-05 09:34:56 +0700

    Update Câu_1.java

--- HN-K24-CNTT5_HaBichNgoc_002/src/Câu_1.java
+++ HN-K24-CNTT5_HaBichNgoc_002/src/Câu_1.java
@@ -2,37 +2,38 @@ import java.util.Scanner;
 
 public class Câu_1 {
 
-    static boolean isLetterAZ(char c){
-        return c>='a' && c<='z';
-    }
+static boolean isLetterAZ(char c){
+    return c>='a' && c<='z';
+}
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập kí tự :");
-        String s = sc.nextLine().toLowerCase();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhập kí tự :");
+    String s = sc.nextLine().toLowerCase();
 
-        int[] count = new int[26];
+    int[] count = new int[26];
 
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(isLetterAZ(c))
-                count[c-'a']++;
+    for(int i=0;i<s.length();i++){
+        char c = s.charAt(i);
+        if(isLetterAZ(c))
+            count[c-'a']++;
         }
 
-        int max=0;
-        char result=' ';
+    int max=0;
+    char result=' ';
 
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(isLetterAZ(c)){
-                if(count[c-'a']>max){
-                    max=count[c-'a'];
-                    result=c;
+    for(int i=0;i<s.length();i++){
+        char c=s.charAt(i);
+        if(isLetterAZ(c)){
+            if(count[c-'a']>max){
+                max=count[c-'a'];
+                result=c;
                 }
             }
         }
 
-        System.out.println(result+" (xuat hien "+max+" lan)");
+    System.out.println(result+" (xuat hien "+max+" lan)");
     }
+
 }
